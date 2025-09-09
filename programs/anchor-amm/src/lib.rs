47449dde7f999bb6db9c41f89781bd89f80d3baa
Byte <bytehash69@gmail.com>
2025-09-10 01:43:43 +0530

    Issues with tests

--- programs/anchor-amm/src/lib.rs
+++ programs/anchor-amm/src/lib.rs
@@ -19,18 +19,18 @@ pub mod anchor_amm {
         Ok(())
     }
 
-    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
-        ctx.accounts.deposit(amount, max_x, max_y)?;
-        Ok(())
-    }
+    // pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+    //     ctx.accounts.deposit(amount, max_x, max_y)?;
+    //     Ok(())
+    // }
 
-    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
-        ctx.accounts.withdraw(amount, max_x, max_y)?;
-        Ok(())
-    }
+    // pub fn withdraw(ctx: Context<Withdraw>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+    //     ctx.accounts.withdraw(amount, max_x, max_y)?;
+    //     Ok(())
+    // }
 
-    pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
-        ctx.accounts.swap(is_x, amount_in, min_amount_out)?;
-        Ok(())
-    }
+    // pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
+    //     ctx.accounts.swap(is_x, amount_in, min_amount_out)?;
+    //     Ok(())
+    // }
 }
--- tests/anchor-amm.ts
+++ tests/anchor-amm.ts
@@ -1,16 +1,73 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorAmm } from "../target/types/anchor_amm";
+import {
+  createMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  getAssociatedTokenAddress,
+} from "@solana/spl-token";
 
 describe("anchor-amm", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.anchorAmm as Program<AnchorAmm>;
+  const connection = provider.connection;
+  const admin = provider.wallet;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  let mintX: anchor.web3.PublicKey;
+  let mintY: anchor.web3.PublicKey;
+  let mintLp: anchor.web3.PublicKey;
+
+  let amm: anchor.web3.PublicKey;
+  let vaultX: anchor.web3.PublicKey;
+  let vaultY: anchor.web3.PublicKey;
+
+  let userAtaX: anchor.web3.PublicKey;
+  let userAtaY: anchor.web3.PublicKey;
+  let userLpAta: anchor.web3.PublicKey;
+
+  const seed = new anchor.BN(8);
+  const fee = 30;
+
+  it("Initialize amm", async () => {
+
+    mintX = await createMint(connection, admin.payer, admin.publicKey, null, 6);
+    mintY = await createMint(connection, admin.payer, admin.publicKey, null, 6);
+
+    [amm] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("amm")],
+      program.programId
+    );
+
+    [mintLp] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("lp"), amm.toBuffer()],
+      program.programId
+    )
+
+    vaultX = await getAssociatedTokenAddress(mintX, admin.publicKey, true);
+    vaultY = await getAssociatedTokenAddress(mintY, admin.publicKey, true);
+
+    // Console log all the variables
+    console.log("mintX:", mintX.toString());
+    console.log("mintY:", mintY.toString());
+    console.log("mintLp:", mintLp.toString());
+    console.log("amm:", amm.toString());
+    console.log("vaultX:", vaultX.toString());
+    console.log("vaultY:", vaultY.toString());
+
+    const tx = await program.methods.initialize(
+      fee,
+      null
+    ).accounts({      
+      admin: admin.publicKey,
+      mintX: mintX,
+      mintY: mintY,
+      systemProgram: anchor.web3.SystemProgram.programId
+    })
+    .rpc();
+
+    console.log(`https://explorer.solana.com/tx/${tx}?cluster=devnet`);
   });
 });
